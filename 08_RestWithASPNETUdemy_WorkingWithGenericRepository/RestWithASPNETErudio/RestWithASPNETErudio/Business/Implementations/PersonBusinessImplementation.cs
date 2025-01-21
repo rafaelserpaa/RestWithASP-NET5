52eaa8fecf02ce68e0e5af02eb306af10d500d9d
rafaelserpaa <97561237+rafaelserpaa@users.noreply.github.com>
2025-01-20 21:14:19 -0300

    Adding Generic Repository

---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETErudio/RestWithASPNETErudio/Business/Implementations/PersonBusinessImplementation.cs
@@ -0,0 +1,50 @@
+﻿using RestWithASPNETErudio.Business;
+using RestWithASPNETErudio.Model;
+using RestWithASPNETErudio.Model.Context;
+using RestWithASPNETErudio.Repository;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RestWithASPNETErudio.Business.Implementations
+{
+    public class PersonBusinessImplementation : IPersonBusiness
+    {
+        private readonly IRepository<Person> _repository;
+
+        public PersonBusinessImplementation(IRepository<Person> repository)
+        {
+            _repository = repository;
+        }
+
+        // Method responsible for returning all people,
+        public List<Person> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        // Method responsible for returning one person by ID
+        public Person FindByID(long id)
+        {
+            return _repository.FindByID(id);
+        }
+
+        // Method responsible to crete one new person
+        public Person Create(Person person)
+        {
+            return _repository.Create(person);
+        }
+
+        // Method responsible for updating one person
+        public Person Update(Person person)
+        {
+            return _repository.Update(person);
+        }
+
+        // Method responsible for deleting a person from an ID
+        public void Delete(long id)
+        {
+           _repository.Delete(id);
+        }
+    }
+}
---
+++ 08_RestWithASPNETUdemy_WorkingWithGenericRepository/RestWithASPNETErudio/RestWithASPNETErudio/Repository/Generic/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNETErudio.Model;
+using RestWithASPNETErudio.Model.Base;
+using System.Collections.Generic;
+
+namespace RestWithASPNETErudio.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T entity);
+        T FindByID(long id);
+        List<T> FindAll();
+        T Update(T entity);
+        void Delete(long id);
+        bool Exists(long id);
+    }
+}
+
